b48d49a55072cf8c2c35ee08c8d4f62407a5f904
Specode <0x0000e000@gmail.com>
2024-12-14 18:43:12 +0800

    first commit

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,21 @@
+-- 设置 leader 键为 ','
+vim.g.mapleader = ',' 
+
+-- Y 当前位置复制整行
+vim.api.nvim_set_keymap('n', 'Y', 'y$', { noremap = true, silent = true })
+
+-- ctrl + t 打开新窗口
+vim.api.nvim_set_keymap('n', '<C-t>', ':tabnew<CR>', { noremap = true, silent = true })
+
+-- 通过 Shift + H/L 在标签页之间切换
+vim.api.nvim_set_keymap('n', '<S-H>', 'gT', { noremap = true, silent = true })  -- Shift+H -> gT
+vim.api.nvim_set_keymap('n', '<S-L>', 'gt', { noremap = true, silent = true })  -- Shift+L -> gt
+
+-- 窗口导航
+vim.api.nvim_set_keymap('n', '<C-k>', '<C-w><Up>', { noremap = true, silent = true })  -- Ctrl+k -> <C-w><Up>
+vim.api.nvim_set_keymap('n', '<C-j>', '<C-w><Down>', { noremap = true, silent = true })  -- Ctrl+j -> <C-w><Down>
+vim.api.nvim_set_keymap('n', '<C-l>', '<C-w><Right>', { noremap = true, silent = true })  -- Ctrl+l -> <C-w><Right>
+vim.api.nvim_set_keymap('n', '<C-h>', '<C-w><Left>', { noremap = true, silent = true })  -- Ctrl+h -> <C-w><Left>
+
+-- nvim-tree 配置
+vim.api.nvim_set_keymap('n', '<C-e>', ':lua require("nvim-tree.api").tree.toggle()<CR>', { noremap = true, silent = true })
---
+++ lua/config/options.lua
@@ -0,0 +1,18 @@
+-- 终端颜色配置
+vim.opt.termguicolors = true
+
+-- 缩进配置
+vim.opt.smarttab = true
+vim.opt.smartindent = true
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = false
+
+-- 剪切板共享
+vim.opt.clipboard = 'unnamedplus'
+
+-- 禁用 netrw 插件
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
