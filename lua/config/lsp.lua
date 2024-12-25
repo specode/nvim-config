d1ca033b76e26d326f23474c07c6b3a19eac0f2c
guozhelin <guozhelin@didiglobal.com>
2024-12-25 20:01:45 +0800

    add format and update live grep keymap

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -21,6 +21,9 @@ local on_attach = function(client, bufnr)
 	vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
 	vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
 	vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
+	vim.keymap.set("n", "<leader>f", function()
+		vim.lsp.buf.format({ async = true }) -- 异步格式化
+	end, opts)
 end
 
 -- 配置 LSP 服务
--- lua/config/telescope.lua
+++ lua/config/telescope.lua
@@ -1,6 +1,6 @@
 local builtin = require("telescope.builtin")
 
 vim.keymap.set("n", "<C-p>", builtin.find_files, { noremap = true, silent = true, desc = "Find Files" })
-vim.keymap.set("n", "<leader>fg", builtin.live_grep, { noremap = true, silent = true, desc = "Live Grep" })
+vim.keymap.set("n", "<C-g>", builtin.live_grep, { noremap = true, silent = true, desc = "Live Grep" })
 vim.keymap.set("n", "<leader>fb", builtin.buffers, { noremap = true, silent = true, desc = "Find Buffers" })
 vim.keymap.set("n", "<leader>fh", builtin.help_tags, { noremap = true, silent = true, desc = "Find Help" })
