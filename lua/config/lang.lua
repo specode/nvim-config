438a02deae281fbe12553a68bba2d93a0569468a
Specode <0x0000e000@gmail.com>
2025-01-06 22:09:42 +0800

    add some lang supports

--- lua/config/lang.lua
+++ lua/config/lang.lua
@@ -2,8 +2,9 @@
 local M = {}
 
 -- https://github.com/neovim/nvim-lspconfig/blob/master/doc/configs.md
--- 具体语言的 LSP 配置在 config/lsp.lua 
-M.lsp_servers = { "lua_ls", "pyright", "gopls", "intelephense" }
+-- 具体语言的 LSP 配置在 config/lsp.lua
+M.lsp_servers = { "lua_ls", "pyright", "gopls", "intelephense", "eslint", "jsonls",
+	"docker_compose_language_service", "dockerls", "volar" }
 
 -- https://github.com/nvim-treesitter/nvim-treesitter?tab=readme-ov-file#supported-languages
 -- M.treesitter_parsers = { "go", "php", "lua", "python", "javascript", "typescript", "html", "css" }
