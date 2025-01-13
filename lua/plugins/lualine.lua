54f17dc2d33d754a58f77822ba0977990e0a8ea3
guozhelin <guozhelin@didiglobal.com>
2025-01-13 17:14:12 +0800

    change colorscheme to tokyonight

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -3,7 +3,11 @@ return {
 		'nvim-lualine/lualine.nvim',
 		version = "*",
 		config = function()
-			require('lualine').setup()
+			require('lualine').setup({
+				options = {
+					theme = 'tokyonight',
+				},
+			})
 		end
 	}
 }
