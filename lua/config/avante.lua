c272be4be4b4e49f7d6824ae62b7146fb84c4a21
guozhelin <guozhelin@didiglobal.com>
2025-02-05 19:21:27 +0800

    update deepseek model

--- lua/config/avante.lua
+++ lua/config/avante.lua
@@ -12,8 +12,7 @@ return {
 	-- openrouter
 	openai = {
 		endpoint = "https://openrouter.ai/api/v1",
-		-- model = "deepseek/deepseek-chat",
-		model = "deepseek/deepseek-r1",
+		model = "deepseek/deepseek-chat",
 		timeout = 30000, -- 超时时间，单位为毫秒
 		temperature = 0,
 		max_tokens = 4096,
