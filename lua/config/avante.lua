4e5c76dc28e6b5c994f890d92b3dbcd0019c7147
Specode <0x0000e000@gmail.com>
2025-03-07 14:07:43 +0800

    Update avante.lua
    add avante provider config

--- lua/config/avante.lua
+++ lua/config/avante.lua
@@ -1,21 +1,41 @@
+-- detail: https://github.com/yetone/avante.nvim/wiki/Custom-providers
 return {
-	provider = "openai",
-	-- deepseek
-	-- openai = {
-	-- 	endpoint = "https://api.deepseek.com",
-	-- 	model = "deepseek-chat",
-	-- 	timeout = 30000, -- 超时时间，单位为毫秒
-	-- 	temperature = 0,
-	-- 	max_tokens = 4096,
-	-- }
-
-	-- openrouter
-	openai = {
-		endpoint = "https://openrouter.ai/api/v1",
-		-- model = "deepseek/deepseek-chat",
-		model = "anthropic/claude-3.7-sonnet",
-		timeout = 30000, -- 超时时间，单位为毫秒
-		temperature = 0,
-		max_tokens = 4096,
-	}
+	provider = 'volcengine',
+	vendors = {
+		volcengine = {
+			__inherited_from = 'openai',
+			endpoint = 'https://ark.cn-beijing.volces.com/api/v3',
+			api_key_name = 'ARK_API_KEY',
+			model = 'deepseek-v3-241226',
+			disable_tools = true,
+		},
+	},
+	-- provider = 'ollama',
+	-- vendors = {
+	-- 	ollama = {
+	-- 		__inherited_from = 'openai',
+	-- 		endpoint = 'http://127.0.0.1:11434/v1',
+	-- 		api_key_name = '',
+	-- 		model = 'codegemma',
+	-- 	},
+	-- },
+	-- provider = 'openrouter',
+	-- vendors = {
+	-- 	openrouter = {
+	-- 		__inherited_from = 'openai',
+	-- 		endpoint = 'https://openrouter.ai/api/v1',
+	-- 		api_key_name = 'OPENROUTER_API_KEY',
+	-- 		-- model = 'deepseek/deepseek-chat',
+	-- 		model = 'anthropic/claude-3.7-sonnet',
+	-- 	},
+	-- },
+	-- provider = 'deepseek',
+	-- vendors = {
+	-- 	deepseek = {
+	-- 		__inherited_from = 'openai',
+	-- 		api_key_name = 'DEEPSEEK_API_KEY',
+	-- 		endpoint = 'https://api.deepseek.com',
+	-- 		model = 'deepseek-coder',
+	-- 	},
+	-- },
 }
