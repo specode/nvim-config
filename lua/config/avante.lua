811b424b33a0ca3dd9057d009bb40d4d698efebe
Null <0x0000e000@gmail.com>
2025-03-14 17:19:58 +0800

    Update avante.lua for support multi providers

--- lua/config/avante.lua
+++ lua/config/avante.lua
@@ -1,41 +1,36 @@
 -- detail: https://github.com/yetone/avante.nvim/wiki/Custom-providers
 return {
-	provider = 'volcengine',
+	provider = 'volcengine_deepseek_v3',
 	vendors = {
-		volcengine = {
+		volcengine_deepseek_v3 = {
 			__inherited_from = 'openai',
 			endpoint = 'https://ark.cn-beijing.volces.com/api/v3',
 			api_key_name = 'ARK_API_KEY',
 			model = 'deepseek-v3-241226',
-			disable_tools = true,
 		},
-	},
-	-- provider = 'ollama',
-	-- vendors = {
-	-- 	ollama = {
-	-- 		__inherited_from = 'openai',
-	-- 		endpoint = 'http://127.0.0.1:11434/v1',
-	-- 		api_key_name = '',
-	-- 		model = 'codegemma',
-	-- 	},
-	-- },
-	-- provider = 'openrouter',
-	-- vendors = {
-	-- 	openrouter = {
-	-- 		__inherited_from = 'openai',
-	-- 		endpoint = 'https://openrouter.ai/api/v1',
-	-- 		api_key_name = 'OPENROUTER_API_KEY',
-	-- 		-- model = 'deepseek/deepseek-chat',
-	-- 		model = 'anthropic/claude-3.7-sonnet',
-	-- 	},
-	-- },
-	-- provider = 'deepseek',
-	-- vendors = {
-	-- 	deepseek = {
-	-- 		__inherited_from = 'openai',
-	-- 		api_key_name = 'DEEPSEEK_API_KEY',
-	-- 		endpoint = 'https://api.deepseek.com',
-	-- 		model = 'deepseek-coder',
-	-- 	},
-	-- },
+		volcengine_deepseek_r1 = {
+			__inherited_from = 'openai',
+			endpoint = 'https://ark.cn-beijing.volces.com/api/v3',
+			api_key_name = 'ARK_API_KEY',
+			model = 'deepseek-r1-250120',
+		},
+		openrouter_claude = {
+			__inherited_from = 'openai',
+			endpoint = 'https://openrouter.ai/api/v1',
+			api_key_name = 'OPENROUTER_API_KEY',
+			model = 'anthropic/claude-3.7-sonnet',
+		},
+		deepseek_coder = {
+			__inherited_from = 'openai',
+			api_key_name = 'DEEPSEEK_API_KEY',
+			endpoint = 'https://api.deepseek.com',
+			model = 'deepseek-coder',
+		},
+		ollama = {
+			__inherited_from = 'openai',
+			endpoint = 'http://127.0.0.1:11434/v1',
+			api_key_name = '',
+			model = 'codegemma',
+		},
+	}
 }
