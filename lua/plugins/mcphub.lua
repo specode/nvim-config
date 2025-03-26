df3156803b36aed084336ac35e4e91f488ea1a9f
Specode <0x0000e000@gmail.com>
2025-03-27 00:04:32 +0800

    disable mcphub

--- lua/plugins/mcphub.lua
+++ lua/plugins/mcphub.lua
@@ -1,11 +1,12 @@
 return {
 	{
 		"ravitemer/mcphub.nvim",
+		enabled = false, -- 添加此选项禁用插件
 		dependencies = {
-			"nvim-lua/plenary.nvim", -- Required for Job and HTTP requests
+			"nvim-lua/plenary.nvim",
 		},
-		-- cmd = "MCPHub", -- lazily start the hub when `MCPHub` is called
-		build = "npm install -g mcp-hub@latest", -- Installs required mcp-hub npm module
+		-- cmd = "MCPHub",
+		build = "npm install -g mcp-hub@latest",
 		config = function()
 			require("config.mcphub").setup()
 		end
