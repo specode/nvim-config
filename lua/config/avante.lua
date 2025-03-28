e7f3ad8f37ae13ee5c8ffec143d014263db148b4
Specode <0x0000e000@gmail.com>
2025-03-28 11:23:23 +0800

    Update avante.lua

--- lua/config/avante.lua
+++ lua/config/avante.lua
@@ -7,14 +7,14 @@ return {
 			endpoint = 'https://ark.cn-beijing.volces.com/api/v3',
 			api_key_name = 'ARK_API_KEY',
 			model = 'deepseek-v3-250324',
-			max_completion_tokens = 12288,
+			max_tokens = 12288,
 		},
 		volcengine_deepseek_r1 = {
 			__inherited_from = 'openai',
 			endpoint = 'https://ark.cn-beijing.volces.com/api/v3',
 			api_key_name = 'ARK_API_KEY',
 			model = 'deepseek-r1-250120',
-			max_completion_tokens = 12288,
+			max_tokens = 12288,
 		},
 		openrouter_claude = {
 			__inherited_from = 'openai',
