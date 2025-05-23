660bb2dfcd3ba9b7363cc178192170e8c01c2dba
Null <0x0000e000@gmail.com>
2025-05-23 10:25:37 +0800

    Update lang.lua

--- lua/config/lang.lua
+++ lua/config/lang.lua
@@ -4,7 +4,7 @@ local M = {}
 -- https://github.com/neovim/nvim-lspconfig/blob/master/doc/configs.md
 -- 具体语言的 LSP 配置在 config/lsp.lua
 M.lsp_servers = { "lua_ls", "pyright", "gopls", "intelephense", "eslint", "jsonls",
-	"docker_compose_language_service", "dockerls", "volar", "ts_ls" }
+	"docker_compose_language_service", "dockerls", "ts_ls" }
 
 -- https://github.com/nvim-treesitter/nvim-treesitter?tab=readme-ov-file#supported-languages
 -- M.treesitter_parsers = { "go", "php", "lua", "python", "javascript", "typescript", "html", "css" }
