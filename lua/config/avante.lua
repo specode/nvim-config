7a38429c391abe64d4d6ff2c97f39b490138b67d
Specode <0x0000e000@gmail.com>
2025-08-23 01:03:52 +0800

    update avante config

--- lua/config/avante.lua
+++ lua/config/avante.lua
@@ -1,7 +1,7 @@
 -- detail: https://github.com/yetone/avante.nvim/wiki/Custom-providers
 return {
 	mode = "agentic",
-	provider = 'aihubmix_gpto4mini',
+	provider = 'deepseek',
 	providers = {
 		aihubmix_gpto4mini = {
 			__inherited_from = 'openai',
@@ -9,22 +9,13 @@ return {
 			api_key_name = 'AIHUBMIX_API_KEY',
 			model = 'o4-mini',
 		},
-		volcengine_deepseek_v3 = {
+		deepseek = {
 			__inherited_from = 'openai',
-			endpoint = 'https://ark.cn-beijing.volces.com/api/v3',
-			api_key_name = 'ARK_API_KEY',
-			model = 'deepseek-v3-250324',
+			endpoint = 'https://api.deepseek.com/v1',
+			api_key_name = 'DEEPSEEK_API_KEY',
+			model = 'deepseek-reasoner',
 			extra_request_body = {
-				max_tokens = 12288,
-			},
-		},
-		volcengine_deepseek_r1 = {
-			__inherited_from = 'openai',
-			endpoint = 'https://ark.cn-beijing.volces.com/api/v3',
-			api_key_name = 'ARK_API_KEY',
-			model = 'deepseek-r1-250120',
-			extra_request_body = {
-				max_tokens = 12288,
+				max_tokens = 8192,
 			},
 		},
 	},
